40baeb8e91d0c980018f62eae6d940ef3bb9606d
Jesse Jaggars <jhjaggars@gmail.com>
2018-12-22 09:30:05 -0600

    adding add

--- cmd/add.go
+++ cmd/add.go
@@ -1,1 +1,22 @@
 package cmd
+
+import (
+	"github.com/jhjaggars/gtl/pkg/todolist"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add an item to the list",
+	Run: func(cmd *cobra.Command, args []string) {
+		tl := todolist.Read(Filename)
+		for _, desc := range args {
+			tl.Add(desc)
+		}
+		tl.Save(Filename)
+	},
+}
